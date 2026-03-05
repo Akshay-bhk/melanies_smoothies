c17ee55d44167740c3c2f1b06aad9b2b511efa28
Akshay-bhk <99667335+Akshay-bhk@users.noreply.github.com>
2026-03-05 15:52:07 +0530

    made request url to show json data

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,4 +37,4 @@ VALUES ('""" + ingredients_string + """','""" + name_on_order + """')"""
 #New Section to display SmoothieFroot Nutrition Information
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
